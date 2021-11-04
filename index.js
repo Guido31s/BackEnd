fc4c232873aa2bbdf51c3f3c06b4e77a820545cf
Guido31s <guidomanusejas@gmail.com>
2021-11-04 16:51:22 -0300

    add chat

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
 const express = require("express");
 const path = require("path")
 const handlebars = require("express-handlebars")
-require("dotenv").config()
-
+let {config} = require("./config")
 const serverRoutes = require("./routes")
+let Sockets = require("./utils/sockets")
+const { Server: HttpServer } = require('http');
 
 // Initializations
 const app = express();
+let httpServer = new HttpServer(app);
+let socket = new Sockets(httpServer);
+socket.listenConnection();
 
 // Settings
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+// EJS----------------------------------------------------
+app.set("views", path.join(__dirname, "views", "ejs"))
+app.set("view engine", "ejs")
+//Pages
+app.use(express.static(path.join(__dirname, "views")));
 
 // PUG----------------------------------------------------
-app.set("views", "./viewPug")
-app.set("view engine", "pug")
+// app.set("views", "./viewPug")
+// app.set("view engine", "pug")
 
 // HBS----------------------------------------------------
 // app.set('views', path.join(__dirname, 'viewHBS', "handlebars"));
 // app.engine('handlebars', handlebars({defaultLayout: 'index' }));
 // app.set('view engine', 'handlebars');
 
-// EJS----------------------------------------------------
-// app.set("views", path.join(__dirname, "views", "ejs"))
-// app.set("view engine", "ejs")
+
 
 
 //Middlewares
 const cors = require("cors");
-app.use(cors("*"));
+app.use(cors(`${config.cors}`));
 
 
-//Pages
-app.use("/productos", express.static(path.join(__dirname, "viewhbs")));
 
 serverRoutes(app);
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Connected to URL:: http://localhost:${process.env.PORT}`)
+httpServer.listen(process.env.PORT, ()=>{
+    console.log(`Connected to URL:: http://localhost:${config.port}`)
 });
 app.on("error", err => console.log("Fallo de conexion al servidor", err));
--- routes/index.js
+++ routes/index.js
@@ -23,63 +23,61 @@ function serverRouter(app) {
         console.log(obj)
         res.redirect("/productosEJS")
     })
+
 // PUG-------------------------------------------
-    router.get("/productosPUG", (req, res) => {
-        let response = contenedor.getAll()
-        res.render("form/form", {response})
-    })
+//     router.get("/productosPUG", (req, res) => {
+//         let response = contenedor.getAll()
+//         res.render("form/form", {response})
+//     })
 
-    router.post("/productosPUG", (req, res) => {
-        let obj = req.body;
-        contenedor.newProd(obj);
-        console.log(obj)
-        res.redirect("/productosPUG")
-    })
-//HBS--------------------------------------------
-router.get("/productosHBS", (req, res) => {
-    let response = contenedor.getAll()
-    res.render("layouts/index", {response})
-})
-router.post("/productosHBS", (req, res) => {
-    let obj = req.body;
-    contenedor.newProd(obj);
-    console.log(obj)
-    res.redirect("/productosHBS")
-})
+//     router.post("/productosPUG", (req, res) => {
+//         let obj = req.body;
+//         contenedor.newProd(obj);
+//         console.log(obj)
+//         res.redirect("/productosPUG")
+//     })
+// //HBS--------------------------------------------
+// router.get("/productosHBS", (req, res) => {
+//     let response = contenedor.getAll()
+//     res.render("layouts/index", {response})
+// })
+// router.post("/productosHBS", (req, res) => {
+//     let obj = req.body;
+//     contenedor.newProd(obj);
+//     console.log(obj)
+//     res.redirect("/productosHBS")
+// })
 
-
-
-
-    // router.get("/api/productos", (req, res)=> {
-    //     let response = contenedor.getAll();
-    //     res.json(response)
-    // })
+//     router.get("/api/productos", (req, res)=> {
+//         let response = contenedor.getAll();
+//         res.json(response)
+//     })
     
-    // router.get("/api/productos/:id", (req, res)=> {
-    //     let { id } = req.params
-    //     let response = contenedor.getProdById(parseInt(id));
-    //     res.json(response)
-    // })
+//     router.get("/api/productos/:id", (req, res)=> {
+//         let { id } = req.params
+//         let response = contenedor.getProdById(parseInt(id));
+//         res.json(response)
+//     })
     
     
-    // router.post("/api/productos", (req, res) =>{
-    //     let obj = req.body;
-    //     let prod = contenedor.newProd(obj)
-    //     res.json(prod)
+//     router.post("/api/productos", (req, res) =>{
+//         let obj = req.body;
+//         let prod = contenedor.newProd(obj)
+//         res.json(prod)
     
-    // })
+//     })
     
-    // router.put("/api/productos/:id", (req, res) => {
-    //     let { id } = req.params
-    //     let response = contenedor.updateProduct(req.query, parseInt(id))
-    //     res.json(response)
-    // })
+//     router.put("/api/productos/:id", (req, res) => {
+//         let { id } = req.params
+//         let response = contenedor.updateProduct(req.query, parseInt(id))
+//         res.json(response)
+//     })
     
-    // router.delete("/api/productos/:id", (req, res)=> {
-    //     let { id } = req.params;
-    //     let response = contenedor.deleteById(parseInt(id));
-    //     res.json(response)
-    // })
+//     router.delete("/api/productos/:id", (req, res)=> {
+//         let { id } = req.params;
+//         let response = contenedor.deleteById(parseInt(id));
+//         res.json(response)
+//     })
     
 }
 
